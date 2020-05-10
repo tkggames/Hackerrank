44170dccfb786e024b46f5e45d6fea1253818900
tkggames <geraldatanga@gmail.com>
2020-05-09 20:14:22 -0700

    add solutions for Hackerrank Simple Array Sum problem

---
+++ src/design/tkggames/hackerrank/SimpleArraySum.java
@@ -0,0 +1,64 @@
+package design.tkggames.hackerrank;
+
+import java.util.stream.IntStream;
+
+public class SimpleArraySum {
+
+    /**
+     * simple for each loop
+     *
+     * @param ar array of integers
+     * @return sum of ar elements
+     */
+    static int simpleArraySumA(int[] ar) {
+        int sum = 0;
+        for (int elem : ar) {
+            sum += elem;
+        }
+        return sum;
+    }
+
+    /**
+     * Two pointer technique
+     *
+     * @param ar array of integers
+     * @return sum of ar elements
+     */
+    static int simpleArraySumB(int[] ar) {
+        int sum = 0;
+        int left = 0, right = ar.length - 1;
+
+        //if ar.length is even
+        while (left < right) {
+            sum += ar[left] + ar[right];
+            ++left;
+            --right;
+        }
+        //if odd, account for middle element
+        if ((ar.length & 1) != 0) {
+            sum += ar[ar.length / 2];
+        }
+        return sum;
+    }
+
+    /**
+     *
+     * @param ar array of integers
+     * @return return sum of ar elements
+     */
+    static int simpleArraySumC(int[] ar) {
+        //uses stream api
+        return IntStream.of(ar).parallel().reduce(0, Integer::sum);
+    }
+
+    public static void main(String[] args) {
+        int[] ar = new int[]{1, 2, 3, 4, 10, 11};
+        printFormat("For Loop", simpleArraySumA(ar));
+        printFormat("Pointers", simpleArraySumB(ar));
+        printFormat("Stream API", simpleArraySumC(ar));
+    }
+
+    private static void printFormat(String description, int result) {
+        System.out.printf("%s : %d%n", description, result);
+    }
+}
