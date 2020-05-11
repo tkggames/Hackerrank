0c014bca16046e4ac3667fc57e22911ef8d268b1
tkggames <geraldatanga@gmail.com>
2020-05-10 19:41:38 -0700

    added ViralAdvertising.java Hackerrank Solution

---
+++ src/design/tkggames/hackerrank/ViralAdvertising.java
@@ -0,0 +1,26 @@
+package design.tkggames.hackerrank;
+
+public class ViralAdvertising {
+    static int viralAdvertising(int n) {
+        int shared, liked = 2, cumulative = 2;
+
+        for(int day = 1; day < n; ++day){
+            shared = liked * 3;
+            liked = shared / 2;
+            cumulative += liked;
+        }
+
+        return cumulative;
+    }
+
+    public static void main(String[] args) {
+        int[] sampleInput = {5, 3};
+        for(int value : sampleInput){
+            printFormat(value, viralAdvertising(value));
+        }
+    }
+
+    private static void printFormat(int val, int result) {
+        System.out.printf("Input: %d = %d%n", val, result);
+    }
+}
